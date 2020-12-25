d191d7e54c87e8a413ed3933b9eefd57e3ca4dee
Abdul-Rahman Abdurrohman <abdul.abdurrohman@hotmail.com>
2020-12-25 01:33:09 +0100

    FInised Sample from Medium Article

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className='home'>
+            <h1>Welcome to Gravity Well Gadgets</h1>
+            <h2>Your One-Stop Shop for Gadgets</h2>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+// https://medium.com/javascript-in-plain-english/react-router-and-spas-made-easy-19fd1db0d6fc
+
+import React from 'react';
+import {Link} from 'react-router-dom'
+const Navbar = ({title}) =>{
+ 
+        return (
+            <nav className="navbar  bg-primary">
+            <h1>
+             {title}
+            </h1>
+            <ul>
+                <li>
+                <Link to='/widgets'>Widgets</Link>
+                </li>
+                <li>
+                <Link to='/wudgets'>Wudgets</Link>
+                </li>
+                <li>
+                <Link to='/'>Home</Link>
+                </li>
+            </ul>
+          </nav>
+        )
+    
+}
+ Navbar.defaultProps={
+  title:'Gravity Well Gadgets'
+};export default Navbar
